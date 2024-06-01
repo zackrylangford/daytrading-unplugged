53138c3b47bb6eb67c1353fd94fe5f9b074ca9b7
Zackry Langford <104527780+zackrylangford@users.noreply.github.com>
2024-06-01 15:12:45 +0000

    updated decimal places to reflect correct amount of places

--- assets/js/table.js
+++ assets/js/table.js
@@ -2,43 +2,43 @@ document.addEventListener("DOMContentLoaded", function() {
     const apiUrl = "https://egu1j7o6aj.execute-api.us-east-1.amazonaws.com/default/return-futures-data";
 
     const tickers = [
-        { symbol: 'ES', name: 'E-mini S&P 500', category: 'CME Equity Futures' },
-        { symbol: 'NQ', name: 'E-mini NASDAQ 100', category: 'CME Equity Futures' },
-        { symbol: 'RTY', name: 'E-mini Russell 2000', category: 'CME Equity Futures' },
-        { symbol: 'NKD', name: 'Nikkei NKD', category: 'CME Equity Futures' },
-        { symbol: '6A', name: 'Australian $', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6B', name: 'British Pound', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6C', name: 'Canadian $', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6E', name: 'Euro FX', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6J', name: 'Japanese Yen', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6S', name: 'Swiss Franc', category: 'CME Foreign Exchange Futures' },
-        { symbol: 'E7', name: 'E-mini Euro FX', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6M', name: 'Mexican Peso', category: 'CME Foreign Exchange Futures' },
-        { symbol: '6N', name: 'New Zealand $', category: 'CME Foreign Exchange Futures' },
-        { symbol: 'HE', name: 'Lean Hogs', category: 'CME Agricultural Futures' },
-        { symbol: 'LE', name: 'Live Cattle', category: 'CME Agricultural Futures' },
-        { symbol: 'CL', name: 'Crude Oil', category: 'CME NYMEX Futures' },
-        { symbol: 'QM', name: 'E-mini Crude Oil', category: 'CME NYMEX Futures' },
-        { symbol: 'NG', name: 'Natural Gas', category: 'CME NYMEX Futures' },
-        { symbol: 'QG', name: 'E-mini Natural Gas', category: 'CME NYMEX Futures' },
-        { symbol: 'RB', name: 'RBOB Gasoline', category: 'CME NYMEX Futures' },
-        { symbol: 'HO', name: 'Heating Oil', category: 'CME NYMEX Futures' },
-        { symbol: 'PL', name: 'Platinum', category: 'CME NYMEX Futures' },
-        { symbol: 'ZC', name: 'Corn', category: 'CME CBOT Agricultural Futures' },
-        { symbol: 'ZW', name: 'Wheat', category: 'CME CBOT Agricultural Futures' },
-        { symbol: 'ZS', name: 'Soybeans', category: 'CME CBOT Agricultural Futures' },
-        { symbol: 'ZM', name: 'Soybean Meal', category: 'CME CBOT Agricultural Futures' },
-        { symbol: 'ZL', name: 'Soybean Oil', category: 'CME CBOT Agricultural Futures' },
-        { symbol: 'YM', name: 'Mini-DOW', category: 'CME CBOT Equity Futures' },
-        { symbol: 'ZT', name: '2-Year Note', category: 'CME CBOT Financial/Interest Rate Futures' },
-        { symbol: 'ZF', name: '5-Year Note', category: 'CME CBOT Financial/Interest Rate Futures' },
-        { symbol: 'ZN', name: '10-Year Note', category: 'CME CBOT Financial/Interest Rate Futures' },
-        { symbol: 'TN', name: '10-Year Ultra-Note', category: 'CME CBOT Financial/Interest Rate Futures' },
-        { symbol: 'ZB', name: '30-Year Bond', category: 'CME CBOT Financial/Interest Rate Futures' },
-        { symbol: 'UB', name: 'Ultra-Bond', category: 'CME CBOT Financial/Interest Rate Futures' },
-        { symbol: 'GC', name: 'Gold', category: 'CME COMEX Futures' },
-        { symbol: 'SI', name: 'Silver', category: 'CME COMEX Futures' },
-        { symbol: 'HG', name: 'Copper', category: 'CME COMEX Futures' }
+        { symbol: 'ES', name: 'E-mini S&P 500', category: 'CME Equity Futures', decimals: 2 },
+        { symbol: 'NQ', name: 'E-mini NASDAQ 100', category: 'CME Equity Futures', decimals: 2 },
+        { symbol: 'RTY', name: 'E-mini Russell 2000', category: 'CME Equity Futures', decimals: 2 },
+        { symbol: 'NKD', name: 'Nikkei NKD', category: 'CME Equity Futures', decimals: 2 },
+        { symbol: '6A', name: 'Australian $', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6B', name: 'British Pound', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6C', name: 'Canadian $', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6E', name: 'Euro FX', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6J', name: 'Japanese Yen', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6S', name: 'Swiss Franc', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: 'E7', name: 'E-mini Euro FX', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6M', name: 'Mexican Peso', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: '6N', name: 'New Zealand $', category: 'CME Foreign Exchange Futures', decimals: 5 },
+        { symbol: 'HE', name: 'Lean Hogs', category: 'CME Agricultural Futures', decimals: 2 },
+        { symbol: 'LE', name: 'Live Cattle', category: 'CME Agricultural Futures', decimals: 2 },
+        { symbol: 'CL', name: 'Crude Oil', category: 'CME NYMEX Futures', decimals: 2 },
+        { symbol: 'QM', name: 'E-mini Crude Oil', category: 'CME NYMEX Futures', decimals: 2 },
+        { symbol: 'NG', name: 'Natural Gas', category: 'CME NYMEX Futures', decimals: 3 },
+        { symbol: 'QG', name: 'E-mini Natural Gas', category: 'CME NYMEX Futures', decimals: 3 },
+        { symbol: 'RB', name: 'RBOB Gasoline', category: 'CME NYMEX Futures', decimals: 3 },
+        { symbol: 'HO', name: 'Heating Oil', category: 'CME NYMEX Futures', decimals: 3 },
+        { symbol: 'PL', name: 'Platinum', category: 'CME NYMEX Futures', decimals: 2 },
+        { symbol: 'ZC', name: 'Corn', category: 'CME CBOT Agricultural Futures', decimals: 2 },
+        { symbol: 'ZW', name: 'Wheat', category: 'CME CBOT Agricultural Futures', decimals: 2 },
+        { symbol: 'ZS', name: 'Soybeans', category: 'CME CBOT Agricultural Futures', decimals: 2 },
+        { symbol: 'ZM', name: 'Soybean Meal', category: 'CME CBOT Agricultural Futures', decimals: 2 },
+        { symbol: 'ZL', name: 'Soybean Oil', category: 'CME CBOT Agricultural Futures', decimals: 2 },
+        { symbol: 'YM', name: 'Mini-DOW', category: 'CME CBOT Equity Futures', decimals: 2 },
+        { symbol: 'ZT', name: '2-Year Note', category: 'CME CBOT Financial/Interest Rate Futures', decimals: 2 },
+        { symbol: 'ZF', name: '5-Year Note', category: 'CME CBOT Financial/Interest Rate Futures', decimals: 2 },
+        { symbol: 'ZN', name: '10-Year Note', category: 'CME CBOT Financial/Interest Rate Futures', decimals: 2 },
+        { symbol: 'TN', name: '10-Year Ultra-Note', category: 'CME CBOT Financial/Interest Rate Futures', decimals: 2 },
+        { symbol: 'ZB', name: '30-Year Bond', category: 'CME CBOT Financial/Interest Rate Futures', decimals: 2 },
+        { symbol: 'UB', name: 'Ultra-Bond', category: 'CME CBOT Financial/Interest Rate Futures', decimals: 2 },
+        { symbol: 'GC', name: 'Gold', category: 'CME COMEX Futures', decimals: 2 },
+        { symbol: 'SI', name: 'Silver', category: 'CME COMEX Futures', decimals: 3 },
+        { symbol: 'HG', name: 'Copper', category: 'CME COMEX Futures', decimals: 4 }
     ];
 
     const fetchAndPopulateTable = async () => {
@@ -65,15 +65,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 row.innerHTML = `
                     <td data-label="Ticker">${ticker.symbol}</td>
                     <td data-label="Name">${ticker.name}</td>
-                    <td data-label="R4">${item.resistance_4.toFixed(2)}</td>
-                    <td data-label="R3">${item.resistance_3.toFixed(2)}</td>
-                    <td data-label="R2">${item.resistance_2.toFixed(2)}</td>
-                    <td data-label="R1">${item.resistance_1.toFixed(2)}</td>
-                    <td data-label="P">${item.pivot_point.toFixed(2)}</td>
-                    <td data-label="S1">${item.support_1.toFixed(2)}</td>
-                    <td data-label="S2">${item.support_2.toFixed(2)}</td>
-                    <td data-label="S3">${item.support_3.toFixed(2)}</td>
-                    <td data-label="S4">${item.support_4.toFixed(2)}</td>
+                    <td data-label="R4">${item.resistance_4.toFixed(ticker.decimals)}</td>
+                    <td data-label="R3">${item.resistance_3.toFixed(ticker.decimals)}</td>
+                    <td data-label="R2">${item.resistance_2.toFixed(ticker.decimals)}</td>
+                    <td data-label="R1">${item.resistance_1.toFixed(ticker.decimals)}</td>
+                    <td data-label="P">${item.pivot_point.toFixed(ticker.decimals)}</td>
+                    <td data-label="S1">${item.support_1.toFixed(ticker.decimals)}</td>
+                    <td data-label="S2">${item.support_2.toFixed(ticker.decimals)}</td>
+                    <td data-label="S3">${item.support_3.toFixed(ticker.decimals)}</td>
+                    <td data-label="S4">${item.support_4.toFixed(ticker.decimals)}</td>
                 `;
                 tableBody.appendChild(row);
             }
